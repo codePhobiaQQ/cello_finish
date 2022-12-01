81fd78a4e61b4a140dee9ca1c49cf1ddc7053000
sergega-fan <sergei_nikitin00@mail.ru>
2022-12-01 11:37:37 +0300

    Fixing popups

--- client/components/popups/PoliticPopup.tsx
+++ client/components/popups/PoliticPopup.tsx
@@ -1,6 +1,4 @@
 import Close from "../UI/close";
-import { AnimatePresence, motion } from "framer-motion";
-import { fadeIn } from "./../../motions/oftenMotions";
 import ReactMarkdown from "react-markdown";
 
 interface IPoliticPopup {
@@ -15,27 +13,12 @@ const PoliticPopup = ({
   text,
 }: IPoliticPopup) => {
   return (
-    <AnimatePresence initial={false}>
-      {isPoliticOpen && (
-        <motion.div
-          variants={fadeIn}
-          animate="visible"
-          initial="hidden"
-          exit="hidden"
-          className="PoliticPopup"
-        >
-          <Close onClick={setPoliticOpen} />
-          <motion.div
-            initial={{ opacity: 0, y: 30 }}
-            animate={{ opacity: 1, y: 0 }}
-            exit={{ opacity: 0, y: 30 }}
-            className="container"
-          >
-            <ReactMarkdown>{text}</ReactMarkdown>
-          </motion.div>
-        </motion.div>
-      )}
-    </AnimatePresence>
+    <div className={`PoliticPopup ${isPoliticOpen ? "active" : ""}`}>
+      <Close onClick={setPoliticOpen} />
+      <div className="container">
+        <ReactMarkdown>{text}</ReactMarkdown>
+      </div>
+    </div>
   );
 };
 
--- client/components/popups/ProtectPopup.tsx
+++ client/components/popups/ProtectPopup.tsx
@@ -1,6 +1,4 @@
 import Close from "../UI/close";
-import { motion, AnimatePresence } from "framer-motion";
-import { fadeIn } from "../../motions/oftenMotions";
 import ReactMarkdown from "react-markdown";
 
 interface IPoliticPopup {
@@ -15,27 +13,12 @@ const ProtectPopup = ({
   text,
 }: IPoliticPopup) => {
   return (
-    <AnimatePresence initial={false}>
-      {isProtectOpen && (
-        <motion.div
-          variants={fadeIn}
-          animate="visible"
-          initial="hidden"
-          exit="hidden"
-          className="PoliticPopup"
-        >
-          <Close onClick={setProtectOpen} />
-          <motion.div
-            initial={{ opacity: 0, y: 30 }}
-            animate={{ opacity: 1, y: 0 }}
-            exit={{ opacity: 0, y: 30 }}
-            className="container"
-          >
-            <ReactMarkdown>{text}</ReactMarkdown>
-          </motion.div>
-        </motion.div>
-      )}
-    </AnimatePresence>
+    <div className={`PoliticPopup ${isProtectOpen ? "active" : ""}`}>
+      <Close onClick={setProtectOpen} />
+      <div className="container">
+        <ReactMarkdown>{text}</ReactMarkdown>
+      </div>
+    </div>
   );
 };
 
