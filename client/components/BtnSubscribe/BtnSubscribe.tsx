fa8178920d2c93e8fef0f13380224ca6c8b8294d
sergega-fan <sergei_nikitin00@mail.ru>
2022-12-04 15:55:29 +0300

    Fixing bottom btn text

--- client/components/BtnSubscribe/BtnSubscribe.tsx
+++ client/components/BtnSubscribe/BtnSubscribe.tsx
@@ -1,19 +1,28 @@
 import styles from "./BtnSubscribe.module.sass";
 import telegram from "../../public/assets/svg/sotials/Telegram.svg";
+import useTypedSelector from "../../hooks/useTypedSelector";
+import { useCallback } from "react";
 
 interface IBtnSubscribe {
   type?: boolean;
   customClass?: string;
-  text?: string;
   clickHandler?: any;
 }
 
-const BtnSubscribe = ({
-  type,
-  customClass,
-  text,
-  clickHandler,
-}: IBtnSubscribe) => {
+const BtnSubscribe = ({ type, customClass, clickHandler }: IBtnSubscribe) => {
+  const lang = useTypedSelector((state) => state.app.language);
+
+  const showBtnText = useCallback(() => {
+    switch (lang) {
+      case "Ru":
+        return "Подписаться";
+      case "En":
+        return "Subscribe";
+      case "De":
+        return "SubscribeDE";
+    }
+  }, [lang]);
+
   return (
     <div className={customClass ? customClass : ""}>
       <button
@@ -23,7 +32,7 @@ const BtnSubscribe = ({
       >
         <div className={styles.subBtn}>
           <img src={telegram.src} alt="telegram" />
-          <span>{text || "Subscribe"}</span>
+          <span>{showBtnText()}</span>
         </div>
       </button>
     </div>
