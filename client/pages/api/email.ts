30cf8e13c4caa38e806db0bec96dc1731148c7e7
sergega-fan <sergei_nikitin00@mail.ru>
2023-02-04 13:27:34 +0300

    Try on server

--- client/pages/api/email.ts
+++ client/pages/api/email.ts
@@ -1,70 +1,66 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from "next";
-import { log } from "util";
+import type { NextApiRequest, NextApiResponse } from 'next'
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const nodemailer = require("nodemailer");
+const nodemailer = require('nodemailer')
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
-  console.log(req.body);
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+	console.log(req.body)
 
-  const transporter = await nodemailer.createTransport({
-    host: "smtp.yandex.ru",
-    port: 465,
-    pool: true,
-    secure: true, // true for 465, false for other ports
-    auth: {
-      user: "ivanskanavi.com@yandex.com", // generated ethereal user
-      // pass: "juk1109@yandex.ru", // generated ethereal password
-      pass: "gpeyubgrpgxkhogb", // generated ethereal password
-    },
-  });
+	const transporter = await nodemailer.createTransport({
+		host: 'imap.gmail.com',
+		port: 993,
+		pool: true,
+		secure: false, // true for 465, false for other ports
+		auth: {
+			user: 'ivanskanavicello@gmail.com', // generated ethereal user
+			// pass: "juk1109@yandex.ru", // generated ethereal password
+			pass: 'Vitalik2007', // generated ethereal password
+		},
+	})
 
-  console.log(transporter);
+	console.log(transporter)
 
-  console.log("log");
+	console.log('log')
 
-  const TitleText = {
-    Ru: "Спасибо за Ваше письмо!",
-    En: "Thank you for you E-Mail!",
-    De: "Vielen Dank für Ihre E-Mail!",
-  };
+	const TitleText = {
+		Ru: 'Спасибо за Ваше письмо!',
+		En: 'Thank you for you E-Mail!',
+		De: 'Vielen Dank für Ihre E-Mail!',
+	}
 
-  const clientText = {
-    Ru: "Спасибо за Ваше письмо! Иван Сканави ответит Вам в ближайшее время.",
-    En: "Thank you for you E-Mail! Ivan Skanavi will be in touch with you shortly",
-    De: "Vielen Dank für Ihre E-Mail! Ivan Skanavi wird sobald wie möglich auf Ihre Nachricht antworten.",
-  };
+	const clientText = {
+		Ru: 'Спасибо за Ваше письмо! Иван Сканави ответит Вам в ближайшее время.',
+		En: 'Thank you for you E-Mail! Ivan Skanavi will be in touch with you shortly',
+		De: 'Vielen Dank für Ihre E-Mail! Ivan Skanavi wird sobald wie möglich auf Ihre Nachricht antworten.',
+	}
 
-  // @ts-ignore
-  const resultText = clientText[req.body.lang];
+	// @ts-ignore
+	const resultText = clientText[req.body.lang]
 
-  console.log(resultText);
+	console.log(resultText)
 
-  const info2 = await transporter.sendMail({
-    from: "ivanskanavi.com@yandex.com", // sender address
-    to: "ivanskanavi.com@yandex.com", // list of receivers
-    subject: "Заявка с сайта ivanskanavi.com", // Subject line
-    html:
-      `<div>${req.body.values.name} - ${req.body.values.email} оставил заявку на сайте</div>` +
-      `<div>Его сообщение:</div>` +
-      `<div>${req.body.values.message}</div>`,
-  });
+	const info2 = await transporter.sendMail({
+		from: 'ivanskanavi.com@yandex.com', // sender address
+		to: 'ivanskanavi.com@yandex.com', // list of receivers
+		subject: 'Заявка с сайта ivanskanavi.com', // Subject line
+		html:
+			`<div>${req.body.values.name} - ${req.body.values.email} оставил заявку на сайте</div>` +
+			`<div>Его сообщение:</div>` +
+			`<div>${req.body.values.message}</div>`,
+	})
 
-  const info1 = await transporter.sendMail({
-    from: "ivanskanavi.com@yandex.com",
-    to: req.body.values.email,
-    // @ts-ignore
-    subject: TitleText[req.body.lang],
-    // @ts-ignore
-    text: resultText,
-    // html: "test",
-  });
+	const info1 = await transporter.sendMail({
+		from: 'ivanskanavi.com@yandex.com',
+		to: req.body.values.email,
+		// @ts-ignore
+		subject: TitleText[req.body.lang],
+		// @ts-ignore
+		text: resultText,
+		// html: "test",
+	})
 
-  console.log(info1);
-  console.log(info2);
+	console.log(info1)
+	console.log(info2)
 
-  res.status(200).json({ data: "success" });
+	res.status(200).json({ data: 'success' })
 }
